a08617866292683f81072eb9ba67122c4d6e49dd
arobertov <gelerob@dir.bg>
2020-03-16 18:25:54 +0200

    Add some features !

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -5,10 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ * @UniqueEntity("name")
  */
 class Category
 {
@@ -20,7 +22,7 @@ class Category
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      * @Assert\NotBlank()
      * @Assert\Length(
      *      min = 2,
@@ -67,6 +69,17 @@ class Category
         return $this->articles;
     }
 
+    /**
+     * @return bool
+     */
+    public function hasArticle(){
+        return !$this->articles->isEmpty();
+    }
+
+    /**
+     * @param Article $article
+     * @return $this
+     */
     public function addArticle(Article $article): self
     {
         if (!$this->articles->contains($article)) {
@@ -77,6 +90,10 @@ class Category
         return $this;
     }
 
+    /**
+     * @param Article $article
+     * @return $this
+     */
     public function removeArticle(Article $article): self
     {
         if ($this->articles->contains($article)) {
@@ -89,7 +106,4 @@ class Category
 
         return $this;
     }
-
-
-
 }
--- src/Migrations/Version20200316142223.php
+++ src/Migrations/Version20200316142223.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200313201629 extends AbstractMigration
+final class Version20200316142223 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -25,9 +25,9 @@ final class Version20200313201629 extends AbstractMigration
         $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, category_id INT NOT NULL, title VARCHAR(255) NOT NULL, contents LONGTEXT NOT NULL, date_created DATETIME NOT NULL, date_edit DATETIME NOT NULL, INDEX IDX_23A0E6612469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE article_article_image (article_id INT NOT NULL, article_image_id INT NOT NULL, INDEX IDX_355199EB7294869C (article_id), INDEX IDX_355199EB684DD106 (article_image_id), PRIMARY KEY(article_id, article_image_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE article_tag (article_id INT NOT NULL, tag_id INT NOT NULL, INDEX IDX_919694F97294869C (article_id), INDEX IDX_919694F9BAD26311 (tag_id), PRIMARY KEY(article_id, tag_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE article_image (id INT AUTO_INCREMENT NOT NULL, image_name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE tag (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_389B7835E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE article_image (id INT AUTO_INCREMENT NOT NULL, image_name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E6612469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
         $this->addSql('ALTER TABLE article_article_image ADD CONSTRAINT FK_355199EB7294869C FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE article_article_image ADD CONSTRAINT FK_355199EB684DD106 FOREIGN KEY (article_image_id) REFERENCES article_image (id) ON DELETE CASCADE');
@@ -42,14 +42,14 @@ final class Version20200313201629 extends AbstractMigration
 
         $this->addSql('ALTER TABLE article_article_image DROP FOREIGN KEY FK_355199EB7294869C');
         $this->addSql('ALTER TABLE article_tag DROP FOREIGN KEY FK_919694F97294869C');
+        $this->addSql('ALTER TABLE article_article_image DROP FOREIGN KEY FK_355199EB684DD106');
         $this->addSql('ALTER TABLE article DROP FOREIGN KEY FK_23A0E6612469DE2');
         $this->addSql('ALTER TABLE article_tag DROP FOREIGN KEY FK_919694F9BAD26311');
-        $this->addSql('ALTER TABLE article_article_image DROP FOREIGN KEY FK_355199EB684DD106');
         $this->addSql('DROP TABLE article');
         $this->addSql('DROP TABLE article_article_image');
         $this->addSql('DROP TABLE article_tag');
+        $this->addSql('DROP TABLE article_image');
         $this->addSql('DROP TABLE category');
         $this->addSql('DROP TABLE tag');
-        $this->addSql('DROP TABLE article_image');
     }
 }
